0c54984f67351c93643ee0f3a6e5b2c64ced418b
Jimmy Gozal <jgozal@developerkit.co>
2016-09-17 13:22:19 -0400

    pushing to avoid conflicts with github pages

--- account-media.js
+++ account-media.js
@@ -1,5 +1,5 @@
  var fs = require('fs');
- var ig = require('instagram-node').instagram();
+ var request = require('sync-request');
 
 // Read full array of top accounts
 
@@ -7,9 +7,15 @@ var topAccountsArray = JSON.parse(fs.readFileSync('top-accounts-array', 'utf8'))
 
 // Instagram Auth
 
+var res = request('GET', 'https://api.instagram.com/v1/users/self/media/recent/?access_token=3925985838.c880b65.5849d24775d848efabe6aa47b746d061');
 
-//var res = request('GET', 'https://api.instagram.com/v1/users/{user-id}/media/recent/?access_token=ACCESS-TOKEN');
+var arr = (JSON.parse(res.getBody().toString()).data);
 
-  
-     
-
+/*
+arr.forEach(function(media){
+    console.log(media.likes.count);
+    console.log(media.comments.count)
+    console.log(media.created_time)
+    console.log(media.user_has_liked)
+    console.log(media)
+})*/
