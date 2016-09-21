1b5190be1616d5e15b98db67a1c86839ad1bd643
Jimmy Gozal <jgozal@developerkit.co>
2016-09-20 23:38:44 -0400

    top media for the day done

--- account-media.js
+++ account-media.js
@@ -1,21 +1,52 @@
- var fs = require('fs');
- var request = require('sync-request');
+var fs = require('fs');
+var request = require('sync-request');
+
+var now = new Date();
+var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+var unixTimestamp = startOfDay / 1000;
+
+var fullArr = [];
 
 // Read full array of top accounts
 
 var topAccountsArray = JSON.parse(fs.readFileSync('top-accounts-array', 'utf8'))
 
-// Instagram Auth
+var getData = function (user) {
 
-var res = request('GET', 'https://api.instagram.com/v1/users/self/media/recent/?access_token=3925985838.c880b65.5849d24775d848efabe6aa47b746d061');
+    var res = request('GET', 'https://www.instagram.com/' + user.account + '/media/');
 
-var arr = (JSON.parse(res.getBody().toString()).data);
+    var data = (JSON.parse(res.getBody().toString()).items);
 
-/*
-arr.forEach(function(media){
-    console.log(media.likes.count);
-    console.log(media.comments.count)
-    console.log(media.created_time)
-    console.log(media.user_has_liked)
-    console.log(media)
-})*/
+    var cleanArr = data.filter(function (media) {
+        return media.created_time >= unixTimestamp;
+    }).map(function (media) {
+        return {
+            user: media.user.username,
+            link: media.link,
+            comments: media.comments.count,
+            likes: media.likes.count,
+            engagement: media.comments.count + media.likes.count
+        }
+    })
+
+    fullArr = fullArr.concat(cleanArr);
+}
+
+// request
+
+topAccountsArray.forEach(function (user) {
+    try {
+        getData(user);
+    } catch (e) {
+        console.log(e);
+    }
+})
+
+
+fullArr.sort(function (obj1, obj2) {
+    return obj2.engagement - obj1.engagement;
+});
+
+
+fs.writeFile("./top-media-array", JSON.stringify(fullArr));
+
