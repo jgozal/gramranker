6c52213ede1062817256adcddb657e2b2eab7e52
Jimmy Gozal <jgozal@developerkit.co>
2016-09-22 22:32:06 -0400

    server set up with routing, getting data on client side!

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(request, response) {
-  request.on('error', function(err) {
-    console.error(err);
-    response.statusCode = 400;
-    response.end();
-  });
-  response.on('error', function(err) {
-    console.error(err);
-  });
-  if (request.method === 'GET' && request.url === '/') {
-    request.pipe(response);
-    var index = fs.readFileSync('./index.html', 'utf8')
-    response.end(index, "utf-8");      
-  } else {
-    response.statusCode = 404;
-    response.end();
+http.createServer(function (req, res) {
+
+  if (req.method === 'GET' && req.url === '/') {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('index.html', 'utf-8', function (err, file) {
+      if (err) {
+        res.end('something went wrong.');
+        return;
+      }
+      res.end(file);
+    });
   }
+
+  if (req.method === 'GET' && req.url === '/data') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    fs.readFile('top-accounts-array', 'utf-8', function (err, file) {
+      if (err) {
+        res.end('something went wrong.');
+        return;
+      }
+      res.end(JSON.stringify(file), 'utf-8');
+    });
+  }
+
+
+
 }).listen(8080);
 console.log("Server running on port 8080.")
 
