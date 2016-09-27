25a16e1a5b499657fb00ca861bcb70bef5f17342
Jimmy Gozal <jgozal@developerkit.co>
2016-09-26 23:46:34 -0400

    post request demo

--- public/render.js
+++ public/render.js
@@ -3,29 +3,31 @@ var fullArr;
 function abbreviateNumber(value) {
     var newValue = value;
     if (value >= 1000) {
-        var suffixes = ["", "k", "m", "b","t"];
-        var suffixNum = Math.floor( (""+value).length/3 );
+        var suffixes = ["", "k", "m", "b", "t"];
+        var suffixNum = Math.floor(("" + value).length / 3);
         var shortValue = '';
         for (var precision = 2; precision >= 1; precision--) {
-            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
-            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            shortValue = parseFloat((suffixNum != 0 ? (value / Math.pow(1000, suffixNum)) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g, '');
             if (dotLessShortValue.length <= 2) { break; }
         }
-        if (shortValue % 1 != 0)  shortNum = shortValue.toFixed(1);
-        newValue = shortValue+suffixes[suffixNum];
+        if (shortValue % 1 != 0) shortNum = shortValue.toFixed(1);
+        newValue = shortValue + suffixes[suffixNum];
     }
     return newValue;
 }
 
-xmlhttp = new XMLHttpRequest();
-xmlhttp.open("GET", "http://localhost:8080/data", false);
-xmlhttp.onreadystatechange = function () {
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-        fullArr = (eval(JSON.parse(xmlhttp.responseText)));
+(function getTop12() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/data", false);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            fullArr = (eval(JSON.parse(xhr.responseText)));
 
+        }
     }
-}
-xmlhttp.send();
+    xhr.send();
+}())
 
 var doc = document.getElementsByClassName('flex-item-12gram');
 fullArr.forEach(function (user, index) {
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser');
 var express = require('express');
 
 var app = express();
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 var staticPath = path.join(__dirname, '/public');
 app.use(express.static(staticPath));
@@ -15,6 +18,11 @@ app.get('/data', function (req, res) {
     }
     res.send(JSON.stringify(eval(JSON.parse(file)).slice(0, 12)));
   });
+});
+
+app.post('/top1000', function(req, res) {
+    console.log('Request for ' + req.body.username + ' was received.');
+    res.end(req.body.username);   
 });
 
 app.listen(8080, function () {
