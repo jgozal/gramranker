3ce91d837184c459636a3264da52211b70c57497
BRS SERVER <jgozal@developerkit.co>
2016-11-22 20:43:19 -0500

    trying to validate user on client side, cross origin issue...

--- public/render.js
+++ public/render.js
@@ -62,30 +62,40 @@ function getTop1000() {
 }
 
 var loadTop1000 = function (rankingArr) {
-    //clear
-    document.getElementById('results').innerHTML = '';
+    //clear results
+    $('#results').html('');
 
-    if (rankingArr.length === 0 && document.getElementById('username').value != '') {
-        $('#results').fadeOut(200, function () {
-            $(this).html('<p>No posts from this user have made it to the top 1000 in the last 24 hours.</p>').fadeIn(200);
-        })
-    } else if (rankingArr.length === 0 && document.getElementById('username').value == '') {
-        $('#results').fadeOut(200, function () {
-            $(this).html('<p>Please enter a username.</p>').fadeIn(200);
-        })
-    } else {
-        $('#results').fadeOut(200, function () {
-            $(this).append('<p><b>' + rankingArr[0].user + ' has ' + rankingArr.length.toString() + ' post/s in the top 1000:</b></p><br>').fadeIn(200);
+    var username = document.getElementById('username').value
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://www.instagram.com/" + username, false);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            if (rankingArr.length === 0 && username != '') {
+                $('#results').fadeOut(100, function () {
+                    $(this).html('<p>No posts from this user have made it to the top 1000 in the last 24 hours.</p>').fadeIn(100);
+                })
+            } else if (rankingArr.length === 0 && username == '') {
+                $('#results').fadeOut(100, function () {
+                    $(this).html('<p>Please enter a username.</p>').fadeIn(100);
+                })
+            } else {
+                $('#results').fadeOut(100, function () {
+                    $(this).append('<p><b>' + rankingArr[0].user + ' has ' + rankingArr.length.toString() + ' post/s in the top 1000:</b></p><br>').fadeIn(100);
 
 
-            rankingArr.forEach(function (media) {
-                $('#results').append('<p><a target="_blank" href="' + media.link + '">This</a> post has made it to the ' + media.ranking + ' so far!<br></p>').fadeIn(200);
-            })
+                    rankingArr.forEach(function (media) {
+                        $('#results').append('<p><a target="_blank" href="' + media.link + '">This</a> post has made it to the ' + media.ranking + ' so far!<br></p>').fadeIn(100);
+                    })
 
-             $(this).append('<ul class="share-buttons"><li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.gramranker.com&t=gramRanker" title="Share on Facebook" target="_blank"><img alt="Share on Facebook" src="images/social/Facebook.svg"></a></li><li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fwww.gramranker.com&text=gramRanker:%20http%3A%2F%2Fwww.gramranker.com" target="_blank" title="Tweet"><img alt="Tweet" src="images/social/Twitter.svg"></a></li><li><a href="https://plus.google.com/share?url=http%3A%2F%2Fwww.gramranker.com" target="_blank" title="Share on Google+"><img alt="Share on Google+" src="images/social/Google+.svg"></a></li><li><a href="http://www.reddit.com/submit?url=http%3A%2F%2Fwww.gramranker.com&title=gramRanker" target="_blank" title="Submit to Reddit"><img alt="Submit to Reddit" src="images/social/Reddit.svg"></a></li><li><a href="mailto:?subject=gramRanker&body=Bringing%20you%20the%20most%20popular%20Instagram%20posts%20daily:%20http%3A%2F%2Fwww.gramranker.com" target="_blank" title="Send email"><img alt="Send email" src="images/social/Email.svg"></a></li></ul>').fadeIn(200);
+                    $(this).append('<ul class="share-buttons"><li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.gramranker.com&t=gramRanker" title="Share on Facebook" target="_blank"><img alt="Share on Facebook" src="images/social/Facebook.svg"></a></li><li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fwww.gramranker.com&text=gramRanker:%20http%3A%2F%2Fwww.gramranker.com" target="_blank" title="Tweet"><img alt="Tweet" src="images/social/Twitter.svg"></a></li><li><a href="https://plus.google.com/share?url=http%3A%2F%2Fwww.gramranker.com" target="_blank" title="Share on Google+"><img alt="Share on Google+" src="images/social/Google+.svg"></a></li><li><a href="http://www.reddit.com/submit?url=http%3A%2F%2Fwww.gramranker.com&title=gramRanker" target="_blank" title="Submit to Reddit"><img alt="Submit to Reddit" src="images/social/Reddit.svg"></a></li><li><a href="mailto:?subject=gramRanker&body=Bringing%20you%20the%20most%20popular%20Instagram%20posts%20daily:%20http%3A%2F%2Fwww.gramranker.com" target="_blank" title="Send email"><img alt="Send email" src="images/social/Email.svg"></a></li></ul>').fadeIn(100);
 
-        })
+                })
+            }
+
+        }
     }
+    xhr.send()
 }
 
 var loadModalContent = function (modal) {
@@ -99,7 +109,7 @@ var loadModalContent = function (modal) {
 
         document.getElementById('mtitle').innerHTML = 'FAQ';
 
-        document.getElementById('mbody').innerHTML = "<p><b>How do you calculate popularity?</b><br><br>Simple: likes + comments.<br><br><b>How often do you update your data?</b><br><br>As the website says, 'every day, every hour.' Although the more correct answer is 'approximately every hour.' Depending on how much media is out there, it can take us up to an hour and twenty minutes to get all of it on each round.<br><br><b>Why do I get results in ranking categories such as 'top 50' or 'top 300' as opposed to getting the exact ranking position of the post?</b><br><br>Instagram is an incredibly dynamic platform. Users like and comment on posts every second of the day and we can only update our data every hour. By the time we tell you a post is ranked 351, it might actually be ranked 334, so we'd rather tell you it made it to the top 400.<br><br><b>What are the different ranking categories?</b><br><br>Top 10, top 30, top 50, top 100, top 200, top 300, top 400, top 500, top 600, top 700, top 800, top 900, and top 1000. If you get results saying that a post made it to the top 1000, you know it ranked somewhere between 900-1000.<br><br><b>Can you enable subscription services so that I can get notified whenever a user of my interest makes it to the top 1000 as opposed to checking the website periodically?</b><br><br>We can, but this takes up server resources and server resources cost money. Shoot us an email at <a href='mailto:info@gramranker.com?Subject=Subscription%20Services' target='_top'>info@gramranker.com</a> and we can chat about it if you're interested.<br><br><b>I'd like to get more data on a specific user's posts.</b><br><br>Shoot us an email at <a href='mailto:info@gramranker.com?Subject=More%20Data' target='_top'>info@gramranker.com</a>.<br><br><b>Do not hesitate to email us if you have any other inquiries.</b></p>"
+        document.getElementById('mbody').innerHTML = "<p><b>How do you calculate popularity?</b><br><br>Simple: likes + comments.<br><br><b>How often do you update your data?</b><br><br>As the website says, 'every day, every hour.' Although the more correct answer is 'approximately every hour.' Sometimes a little less, sometimes a little more, depending on how much media is out there.<br><br><b>Why do I get results in ranking categories such as 'top 50' or 'top 300' as opposed to getting the exact ranking position of the post?</b><br><br>Instagram is an incredibly dynamic platform. Users like and comment on posts every second of the day and we can only update our data every hour. By the time we tell you a post is ranked 351, it might actually be ranked 334, so we'd rather tell you it made it to the top 400.<br><br><b>What are the different ranking categories?</b><br><br>Top 10, top 30, top 50, top 100, top 200, top 300, top 400, top 500, top 600, top 700, top 800, top 900, and top 1000. If you get results saying that a post made it to the top 1000, you know it ranked somewhere between 900-1000.<br><br><b>Can you enable subscription services so that I can get notified whenever a user of my interest makes it to the top 1000 as opposed to checking the website periodically?</b><br><br>We can, but this takes up server resources and server resources cost money. Shoot us an email at <a href='mailto:info@gramranker.com?Subject=Subscription%20Services' target='_top'>info@gramranker.com</a> and we can chat about it if you're interested.<br><br><b>I'd like to get more data on a specific user's posts.</b><br><br>Shoot us an email at <a href='mailto:info@gramranker.com?Subject=More%20Data' target='_top'>info@gramranker.com</a>.<br><br><b>Do not hesitate to email us if you have any other inquiries.</b></p>"
     }
 }
 
