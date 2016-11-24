8c7bfc23462bd22f3151e2b9bfbf311cbff1618b
Jimmy Gozal <jgozal@developerkit.co>
2016-11-24 18:22:30 -0500

    changed some native js to jquery to keep things consistent

--- public/render.js
+++ public/render.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
         });
 
     // decorate top Instagram posts
-    var top12 = document.getElementsByClassName('flex-item-12gram');
+    var top12 = $('.flex-item-12gram');
     fullArr.forEach(function (user, index) {
         if (user.type === "image") {
             top12[index].parentElement.setAttribute('href', user.link);
@@ -40,7 +40,7 @@ $(document).ready(function () {
 
 // check if user is in top 1000
 function getTop1000() {
-    var username = document.getElementById('username').value.toLowerCase()
+    var username = $('#username').val().toLowerCase()
     //clear results
     $('#results').html('');
     //check if user exists
@@ -96,15 +96,15 @@ var loadTop1000 = function (rankingArr, username) {
 var loadModalContent = function (modal) {
     if (modal === 'Privacy Policy') {
 
-        document.getElementById('mtitle').innerHTML = 'Privacy Policy';
+        $('#mtitle').html('Privacy Policy');
 
-        document.getElementById('mbody').innerHTML = "<p>We collect the following information:<br><br><b>Queries:</b> gramRanker may keep track of the Instagram usernames being queried through our application. We do this to help improve our services, to provide more relevant advertising, and to be able to share aggregate statistics such as the frequency of a particular user being queried.<br><br><b>Cookies:</b> Like most apps, we use cookies and similar technologies to collect additional application usage data and to improve our application. gramRanker may use both session cookies and persistent cookies to better understand how our users interact with our application, to monitor aggregate usage by our users and traffic routing on our application, and to customize and improve our services.<br><br><b>Google Analytics:</b> we use Google Analytics to analyze traffic behavior when using our application.<br><br><b>No personal information is collected/stored/distributed through any of our services. Do not hesitate to contact us at <a href='mailto:info@gramranker.com?Subject=Privacy%20Policy' target='_top'>info@gramranker.com</a> if you have any questions.</b></p>"
+        $('#mbody').html("<p>We collect the following information:<br><br><b>Queries:</b> gramRanker may keep track of the Instagram usernames being queried through our application. We do this to help improve our services, to provide more relevant advertising, and to be able to share aggregate statistics such as the frequency of a particular user being queried.<br><br><b>Cookies:</b> Like most apps, we use cookies and similar technologies to collect additional application usage data and to improve our application. gramRanker may use both session cookies and persistent cookies to better understand how our users interact with our application, to monitor aggregate usage by our users and traffic routing on our application, and to customize and improve our services.<br><br><b>Google Analytics:</b> we use Google Analytics to analyze traffic behavior when using our application.<br><br><b>No personal information is collected/stored/distributed through any of our services. Do not hesitate to contact us at <a href='mailto:info@gramranker.com?Subject=Privacy%20Policy' target='_top'>info@gramranker.com</a> if you have any questions.</b></p>");
 
     } else if (modal === 'FAQ') {
 
-        document.getElementById('mtitle').innerHTML = 'FAQ';
+        $('#mtitle').html('FAQ');
 
-        document.getElementById('mbody').innerHTML = "<p><b>How do you calculate popularity?</b><br><br>Simple: likes + comments.<br><br><b>How often do you update your data?</b><br><br>As the website says, 'every day, every hour.' Although the more correct answer is 'approximately every hour.' Sometimes a little less, sometimes a little more, depending on how much media is out there.<br><br><b>Why do I get results in ranking categories such as 'top 50' or 'top 300' as opposed to getting the exact ranking position of the post?</b><br><br>Instagram is an incredibly dynamic platform. Users like and comment on posts every second of the day and we can only update our data every hour. By the time we tell you a post is ranked 351, it might actually be ranked 334, so we'd rather tell you it made it to the top 400.<br><br><b>What are the different ranking categories?</b><br><br>Top 10, top 30, top 50, top 100, top 200, top 300, top 400, top 500, top 600, top 700, top 800, top 900, and top 1000. If you get results saying that a post made it to the top 1000, you know it ranked somewhere between 900-1000.<br><br><b>Can you enable subscription services so that I can get notified whenever a user of my interest makes it to the top 1000 as opposed to checking the website periodically?</b><br><br>We can, but this takes up server resources and server resources cost money. Shoot us an email at <a href='mailto:info@gramranker.com?Subject=Subscription%20Services' target='_top'>info@gramranker.com</a> and we can chat about it if you're interested.<br><br><b>I'd like to get more data on a specific user's posts.</b><br><br>Shoot us an email at <a href='mailto:info@gramranker.com?Subject=More%20Data' target='_top'>info@gramranker.com</a>.<br><br><b>Do not hesitate to email us if you have any other inquiries.</b></p>"
+        $('#mbody').html("<p><b>How do you calculate popularity?</b><br><br>Simple: likes + comments.<br><br><b>How often do you update your data?</b><br><br>As the website says, 'every day, every hour.' Although the more correct answer is 'approximately every hour.' Sometimes a little less, sometimes a little more, depending on how much media is out there.<br><br><b>Why do I get results in ranking categories such as 'top 50' or 'top 300' as opposed to getting the exact ranking position of the post?</b><br><br>Instagram is an incredibly dynamic platform. Users like and comment on posts every second of the day and we can only update our data every hour. By the time we tell you a post is ranked 351, it might actually be ranked 334, so we'd rather tell you it made it to the top 400.<br><br><b>What are the different ranking categories?</b><br><br>Top 10, top 30, top 50, top 100, top 200, top 300, top 400, top 500, top 600, top 700, top 800, top 900, and top 1000. If you get results saying that a post made it to the top 1000, you know it ranked somewhere between 900-1000.<br><br><b>Can you enable subscription services so that I can get notified whenever a user of my interest makes it to the top 1000 as opposed to checking the website periodically?</b><br><br>We can, but this takes up server resources and server resources cost money. Shoot us an email at <a href='mailto:info@gramranker.com?Subject=Subscription%20Services' target='_top'>info@gramranker.com</a> and we can chat about it if you're interested.<br><br><b>I'd like to get more data on a specific user's posts.</b><br><br>Shoot us an email at <a href='mailto:info@gramranker.com?Subject=More%20Data' target='_top'>info@gramranker.com</a>.<br><br><b>Do not hesitate to email us if you have any other inquiries.</b></p>");
     }
 }
 
